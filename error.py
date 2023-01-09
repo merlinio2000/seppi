9f49c59b036a2b232bcb6091ea7aa795b1f97dbd
Merlin Maggi <maggimer@students.zhaw.ch>
2023-01-09 16:36:47 +0100

    add jacobi & gauss-seidel

--- error.py
+++ error.py
@@ -2,16 +2,16 @@ import numpy as np
 from utl import assert_eq_shape
 
 
-def err_priori(n: int, alpha: np.float64, x1: np.ndarray, x0: np.ndarray):
+def err_priori(n: int, alpha: float, x1: np.ndarray, x0: np.ndarray):
    assert_eq_shape(x1, x0)
    return ((alpha**n) / (1-alpha)) * np.linalg.norm(x1 - x0, np.inf)
     
-def apriori_n_steps(tol: np.float64, alpha: np.float64, x1: np.ndarray, x0: np.ndarray):
+def apriori_n_steps(tol: float, alpha: float, x1: np.ndarray, x0: np.ndarray):
     assert_eq_shape(x1, x0)
     norm = np.linalg.norm(x1 - x0, np.inf)
     return np.log((tol * (1-alpha)) / norm) / np.log(alpha)
 
-def err_posteriori(alpha: np.float64, x1: np.ndarray, x0: np.ndarray):
+def err_posteriori(alpha: float, x1: np.ndarray, x0: np.ndarray):
     assert_eq_shape(x1, x0)
     norm = np.linalg.norm(x1 - x0, np.inf)
     return (alpha / (1 - alpha)) * norm
--- lineare_glg.py
+++ lineare_glg.py
@@ -1,7 +1,9 @@
+from typing import Literal, Union
 import numpy as np
 import scipy.linalg as spl
 
-from utl import assert_square
+from utl import assert_square, assert_dimensions_match, assert_eq_shape, assert_is_vec
+from error import apriori_n_steps
 
 
 def np_RLP_zerlegung(A: np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
@@ -57,10 +59,83 @@ def QR_zerlegung(A: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     return (Q,R)
 
 
+def gaussSeidel_or_jacobi(A: np.ndarray, b: np.ndarray, x0: np.ndarray, \
+                    tol: float = 1e-9, \
+                    opt: Union[Literal['g'], Literal['j']] = 'g') -> tuple[np.ndarray, int, float]:
+    """
+    Gauss-Seidel oder Jacobi verfahren für das System Ax = b, DEFAULT = Gauss-Seidel
+
+    Parameters:
+        A: n x n Matrix
+        b: Vektor aus Gleichungssystem
+        x0: Startvektor für die iteration
+        tol = 1e-9: Fehlertoleranz für die Lösung
+        opt = 'g': Modus; 'g' / 'j' respektive für Gauss-Seidel/Jacobi
+    Returns:
+        x_n: Lösungsvektor x nach n Iterationen
+        n: Anzahl der durchgeführten Iterationen
+        expected_n: Anzahl der erwarteten Iterationen aus Formel
+    """    
+
+    assert_square(A)
+    assert_is_vec(b)
+    assert_is_vec(x0)
+    assert_dimensions_match(A, b)
+    assert_eq_shape(b, x0)
+
+    A = A.astype(np.float64)
+    b = b.astype(np.float64)
+    x0 = x0.astype(np.float64)    
+
+    R = np.triu(A, 1)
+    L = np.tril(A, -1)
+    D = np.diagflat(np.diag(A))
+    
+    B = None
+    C = None
+    
+    if (opt == 'j'): # Jacobi
+        D_inv = np.linalg.inv(D)
+        B = -D_inv @ (L + R)
+        C = D_inv @ b
+    elif (opt == 'g'): # Gauss-Seidel
+        DL_inv = np.linalg.inv(D + L)
+        B = -DL_inv @ R
+        C = DL_inv @ b
+    else:
+        raise Exception(f'Unknown mode {opt}')
+    
+    def step(xk): return B @ xk + C
+
+    alpha: float = np.linalg.norm(B, np.inf) # type: ignore
+    if (alpha >= 1):
+        raise Exception('Kein Abbruchkriterium möglich/abstossender Fixpunkt')
+
+    x_prev = x0
+    x_curr = step(x0)
+
+    expected_n = apriori_n_steps(tol, alpha, x_curr, x_prev)
+
+    err_fact = alpha / (1 - alpha)
+    
+    n_steps = 1 # wir haben bereits eine Iteration um expected_n zu berechnen können
+    while err_fact * np.linalg.norm(x_curr - x_prev, np.inf) > tol:
+        x_prev = x_curr
+        x_curr = step(x_curr)
+        n_steps += 1
+    
+    return x_curr, n_steps, expected_n 
+
+
+
+
+
+
+
 import unittest
 
 class LineareGlgTest(unittest.TestCase):
-    def test_S8_A2(self):
+    def test_QR_S8_A2(self):
         A = np.array([[1, -2, 3],
                     [-5, 4, 1],
                     [2, -1, 3]], dtype=np.float64)
@@ -80,6 +155,22 @@ class LineareGlgTest(unittest.TestCase):
 
         _, _, P = np_RLP_zerlegung(A)
         self.assertTrue(np.allclose(P, np.eye(3)))
+    
+    def test_gaussSeidel_S10_A2a(self):
+        A = np.array([[8, 5, 2],
+                [5, 9, 1],
+                [4, 2, 7]], dtype=np.float64)
+
+        b = np.array([19, 5, 34], dtype=np.float64)
+
+        x0 = np.array([1, -1, 3])
+
+        x_n, _, expected_n  = gaussSeidel_or_jacobi(A, b, x0, 1e-4)
+        expected_x = np.array([2.00000527, -1.00000223, 3.99999763])
+
+        self.assertTrue(np.allclose(x_n, expected_x))
+        self.assertAlmostEqual(expected_n, 86.22, places=2)
+
 
 
 if __name__ == '__main__':
