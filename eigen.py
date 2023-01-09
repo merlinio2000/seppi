d8dfe6e81da26f1e40af87f6a0cefe9e3ea2dcf0
Merlin Maggi <maggimer@students.zhaw.ch>
2023-01-09 12:55:06 +0100

    improvements to the eigen-analysis

--- eigen.py
+++ eigen.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
+import math
 import numpy as np
-from seppi.utl import is_symmetric
-
-from utl import assert_dimensions_match, assert_normed, assert_square
+from utl import assert_dimensions_match, assert_normed, assert_square, is_symmetric
 
 
 def eigen_QR(A: np.ndarray, max_iter: int) -> tuple[np.ndarray, np.ndarray]:
@@ -38,59 +37,114 @@ class EigenQRAnalysisResult:
 
     Attributes:
         A_symmetrisch: ob die Eingabematrix A symmetrisch_ist
+
         B_ist_D: Ob die Matrix B = T^-1 A T diagonal ist 
                 (in diesem Fall spricht mann in Formeln von D = B)
+
+        komplexe_EW: Ob die Matrix B komplexe Eigenwerte in 2x2 Blöcken enthält
+
         EV_sind_spalten_von_T: kann nur True sein falls #matrix_ist_diagonal || #A_symmetrisch = True
                 Falls True können Eigenvektoren aus Spalten von T abgelesen werden
                 Dies ist der Fall wenn A diagonalisierbar (== #B_ist_D)
                 oder A symmetrisch ist und alle Elemente der Diagonale **betragsmässig** verschieden sind 
     """
-    A_symmetrisch: bool = False
-    B_ist_D: bool = False
-    EV_sind_spalten_von_T: bool = False
+    A_symmetrisch: bool | None = None 
+    B_ist_D: bool | None = None
+    komplexe_EW: bool | None = None
+    EV_sind_spalten_von_T: bool | None = None
 
 
-def eigen_QR_analysis(A, max_iter=100) -> EigenQRAnalysisResult:
+def eigen_QR_analysis(A: np.ndarray, max_iter: int = 100, tol: float = 1e-9) -> EigenQRAnalysisResult:
     assert_square(A)
 
+    np.set_printoptions(linewidth=150)
+
     A_symmetrisch = is_symmetric(A)
+
+    # np_ews[i] -> np_evs[:, i] für zugehörigen EV
+    np_ews, np_evs = np.linalg.eig(A)
+    print("Eigenwerte nach numpy:")
+    print(np_ews)
+    print("normierte Eigenvektoren nach numpy: (Eigenwertindex entspricht Spaltenindex des zugehörigen EV)")
+    print(np_evs)
 
     B, T = eigen_QR(A, max_iter)
 
     n = B.shape[0]
     
-    # Test für komplexe Eigenwerte
+    # Test ob B diagonal (D = B) 
     is_diag_matrix = True
-    for i in range(n):
-        for k in range(n):
-            if i != k:
-                is_diag_matrix = np.isclose(B[i, k], 0)
+    for col in range(n):
+        for row in range(n):
+            if col != row:
+                is_diag_matrix = math.isclose(B[col, row], 0, abs_tol=tol)
+            if not is_diag_matrix:
+                break
+        if not is_diag_matrix:
+            break
 
-    if not is_diag_matrix: # TODO correct
-        print("Matrix scheint komplexe Eigenwerte zu haben, manuell prüfen")
-        return
-    D = B # für konsistenz mit mathematischer Schreibweise
-    print("Eigenwerte stehen auf der Diagonalen von Folgender Matrix")
-    print(D)
+    if is_diag_matrix:
+        print("Eigenwerte stehen auf der Diagonalen von folgender Matrix D = B")
+        print(B)
+        print("unabhängige Eigenvektoren stehen in den Spalten folgender Matrix T")
+        print(T)
+        return EigenQRAnalysisResult(A_symmetrisch=A_symmetrisch, \
+                                        B_ist_D=True, \
+                                        komplexe_EW=False, \
+                                        EV_sind_spalten_von_T=True)
+
+
+
+
+    # Test ob keine != 0 unter der Diagonale ("perfekte" obere Dreiecksmatrix)
+    # Falls dem nicht so ist, treten komplexe Eigenwerte in 2x2 Blöcken auf
+    zero_below_diagonal = True
+    for col in range(n):
+        for row in range(col+1, n):
+            zero_below_diagonal = math.isclose(B[row, col], 0, abs_tol=tol)
+            if not zero_below_diagonal:
+                break
+        if not zero_below_diagonal:
+            break
+    
+    if not zero_below_diagonal:
+        print("In folgender Matrix treten komplexe Eigenwerte als 2x2 Matrizen auf, manuell prüfen")
+        print(B)
+        return EigenQRAnalysisResult(A_symmetrisch=A_symmetrisch, \
+                                        B_ist_D=False, \
+                                        komplexe_EW=True, \
+                                        EV_sind_spalten_von_T=False)
+    else:
+        print("Eigenwerte stehen auf der Diagonalen von folgender Matrix B")
+        print(B)
+
 
     # Test ob alle Eigenwerte betragsmässig verschieden sind
-    seen_ews = []
+    # Zu diesem Zeitpunkt is bereit geprüft, dass
+    #   keine komplexen EW (B ist obere Dreiecksmatrix)
+    #   B ist nicht diagonal
+    seen_abs_ews = []
     all_absolute_different = True
-    for ew in np.diag(D):
+    for ew in np.diag(B):
         abs_ew = np.abs(ew)
-        for seen_ew in seen_ews:
-            if np.isclose(abs_ew, seen_ew):
-                all_absolute_different = False
-                break
+        all_absolute_different = not any(math.isclose(seen_abs_ew, abs_ew, abs_tol=tol) for seen_abs_ew in seen_abs_ews)
+        seen_abs_ews.append(abs_ew)
         if not all_absolute_different:
             break
 
     if all_absolute_different and A_symmetrisch:
-        print("Eigenvektoren stehen in den Spalten folgender Matrix T")
+        print("unabhängige Eigenvektoren stehen in den Spalten folgender Matrix T")
         print(T)
-        return(A_symmetrisch, B_ist_D)
+        return EigenQRAnalysisResult(A_symmetrisch=True, \
+                                        B_ist_D=False, \
+                                        komplexe_EW=False, \
+                                        EV_sind_spalten_von_T=True)
     else:
         print("Eigenvektoren müssen manuell bestimmt werden")
+        return EigenQRAnalysisResult(A_symmetrisch=A_symmetrisch, \
+                                        B_ist_D=False, \
+                                        komplexe_EW=False, \
+                                        EV_sind_spalten_von_T=False)
     
 
 
@@ -133,18 +187,34 @@ class EigenTest(unittest.TestCase):
         A = np.array([[1, -2, 0],
                     [2, 0, 1],
                     [0, -2, 1]], dtype=np.float64)
-        D, _ = eigen_QR(A, 100)
+        B, _ = eigen_QR(A, 100)
+        analysis = eigen_QR_analysis(A)
 
         expected = np.array([[-0.1072, -2.5543, -0.6586],
                         [2.3955, 1.1072, -0.2575],
                         [0, 0, 1]])
-        self.assertTrue(np.allclose(D, expected, atol=1e-04))
+        self.assertTrue(np.allclose(B, expected, atol=1e-04))
+
+
+        self.assertFalse(analysis.A_symmetrisch)
+        self.assertFalse(analysis.B_ist_D)
+        self.assertTrue(analysis.komplexe_EW)
+        self.assertFalse(analysis.EV_sind_spalten_von_T)
 
     def test_HS2020_A6(self):
         A = np.array([[13, -4],
                     [30, -9]], dtype=np.float64)
-        D, _ = eigen_QR(A, 100)
-        eigen_values
+        B, _ = eigen_QR(A, 100)
+        analysis = eigen_QR_analysis(A)
+
+        self.assertAlmostEqual(B[0, 0], 3.)
+        self.assertAlmostEqual(B[1, 1], 1.)
+
+        self.assertFalse(analysis.A_symmetrisch)
+        self.assertFalse(analysis.B_ist_D)
+        self.assertFalse(analysis.komplexe_EW)
+        self.assertFalse(analysis.EV_sind_spalten_von_T)
+        
 
 if __name__ == '__main__':
     unittest.main()
