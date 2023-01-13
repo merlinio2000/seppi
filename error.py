ffe8de5e86507eb30440de1b00ced06c84e7f92c
Merlin Maggi <maggimer@students.zhaw.ch>
2023-01-13 17:22:06 +0100

    formeln für Fehler in Ax=b

--- error.py
+++ error.py
@@ -1,5 +1,5 @@
 import numpy as np
-from utl import assert_eq_shape
+from utl import assert_eq_shape, assert_is_vec, assert_square, assert_dimensions_match
 
 
 def err_priori(n: int, alpha: float, x1: float, x0: float):
@@ -28,3 +28,63 @@ def err_posteriori_vec(alpha: float, x_curr: np.ndarray, x_prev: np.ndarray):
     assert_eq_shape(x_curr, x_prev)
     norm = np.linalg.norm(x_curr - x_prev, np.inf)
     return (alpha / (1 - alpha)) * norm
+
+
+
+"""
+Funktionen für die Fehler aus linearen Gleichungsystemen Ax=b
+"""
+
+def err_abs_b_fehler_A_exakt(A: np.ndarray, b: np.ndarray, b_err: np.ndarray) -> float:
+    """
+    Für den Fall das in der Gleichung Ax=b NUR b fehlerhaft ist
+    """
+    assert_square(A)
+    assert_is_vec(b)
+    assert_is_vec(b_err)
+    assert_eq_shape(b, b_err)
+    assert_dimensions_match(A, b)
+
+    return np.linalg.norm(np.linalg.inv(A), np.inf) * np.linalg.norm(b - b_err, np.inf) # type: ignore
+
+def err_rel_b_fehler_A_exakt(A: np.ndarray, b: np.ndarray, b_err: np.ndarray) -> float:
+    """
+    Für den Fall das in der Gleichung Ax=b NUR b fehlerhaft ist
+    """
+    assert_square(A)
+    assert_is_vec(b)
+    assert_is_vec(b_err)
+    assert_eq_shape(b, b_err)
+    assert_dimensions_match(A, b)
+
+    if np.linalg.norm(b, np.inf) == 0:
+        raise Exception('Division durch norm = 0 nicht erlaubt')
+    cond = np.linalg.cond(A, np.inf)
+    return cond * (np.linalg.norm(b-b_err, np.inf) / np.linalg.norm(b, np.inf))
+
+
+def err_rel_b_fehler_A_fehler(A: np.ndarray, A_err: np.ndarray,  b: np.ndarray, b_err: np.ndarray) -> float:
+    """
+    Für den Fall das in der Gleichung Ax=b sowohl A als auch B fehlerhaft ist
+    """
+    assert_square(A)
+    assert_eq_shape(A, A_err)
+    assert_is_vec(b)
+    assert_is_vec(b_err)
+    assert_eq_shape(b, b_err)
+    assert_dimensions_match(A, b)
+
+
+    if np.linalg.norm(b, np.inf) == 0 or np.linalg.norm(A, np.inf): 
+        raise Exception('Division durch norm = 0 nicht erlaubt')
+
+    cond = np.linalg.cond(A)
+
+    err_fact_A = np.linalg.norm(A-A_err, np.inf) / np.linalg.norm(A, np.inf)
+
+    if cond * err_fact_A >= 1:
+        raise Exception('Formel für diese Matrix A nicht anwendbar')
+
+    err_fact_b = np.linalg.norm(b-b_err, np.inf)/np.linalg.norm(b, np.inf)
+
+    return cond/(1-(cond * err_fact_A)) * (err_fact_A + err_fact_b) 
